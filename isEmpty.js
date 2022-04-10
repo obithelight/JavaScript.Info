e952f6b38d7e755dbbed0a792b41b5e35cbc3ccb
Kenneth Onu <kennetho.onu@gmail.com>
2022-04-10 11:53:17 +0800

    Check if an Object is Empty

--- isEmpty.js
+++ isEmpty.js
@@ -18,3 +18,11 @@ function isEmpty(obj) {
   }
   return true;
 }
+
+let schedule = {};
+
+console.log(isEmpty(schedule)); // true
+
+schedule["8:30"] = "get up";
+
+console.log(isEmpty(schedule)); // false
