3be7565ba9ce6f615fa3c9c1f42b1f96e35cd3b2
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-26 14:40:10 +0800

    Create a Calculator Object

---
+++ new-calculator.js
@@ -0,0 +1,10 @@
+/*
+Create new Calculator
+importance: 5
+Create a constructor function Calculator that creates objects with 3 methods:
+
+read() prompts for two values and saves them as object properties with names a and b respectively.
+sum() returns the sum of these properties.
+mul() returns the multiplication product of these properties.
+For instance:
+*/
