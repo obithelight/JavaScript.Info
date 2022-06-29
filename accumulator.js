bf7bfa364b95ba633e2343211d142ffb295e0a07
Kenneth Onu <kennetho.onu@gmail.com>
2022-06-29 17:13:11 +0800

    Create an Accumulator Property Value

--- accumulator.js
+++ accumulator.js
@@ -1,6 +1,5 @@
-/*
-Create new Accumulator
-
+/*Create new Accumulator
+importance: 5
 Create a constructor function Accumulator(startingValue).
 
 Object that it creates should:
@@ -8,8 +7,9 @@ Object that it creates should:
 Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
 The read() method should use prompt to read a new number and add it to value.
 In other words, the value property is the sum of all user-entered values with the initial value startingValue.
-*/
 
+Here’s the demo of the code:
+*/
 class Accumulator {
   constructor(startingValue) {
     this.value = startingValue;
