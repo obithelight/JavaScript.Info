604e15ec3d9a298b2663858fca24041814e98eee
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-24 08:27:01 +0800

    Chaining in OOP

--- chaining.js
+++ chaining.js
@@ -4,3 +4,15 @@ importance: 2
 There’s a ladder object that allows to go up and down:
 */
 
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+  },
+  down() {
+    this.step--;
+  },
+  showStep: function() { // shows the current step
+    alert( this.step );
+  }
+};
