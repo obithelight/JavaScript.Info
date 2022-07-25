7ce4889d9b2ecec50a97107c0e5b742e7621b270
Kenneth Onu <kennetho.onu@gmail.com>
2022-07-25 08:28:57 +0800

    Chaining in OOP

--- chaining.js
+++ chaining.js
@@ -19,3 +19,11 @@ let ladder = {
 
 //Now, if we need to make several calls in sequence, can do it like this:
 
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+ladder.down();
+ladder.showStep(); // 0
+Modify the code of up, down and showStep to make the calls chainable, like this:
+
