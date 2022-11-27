acbb85d46729727d4e9535d65a341908d40dbbe1
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-28 07:41:54 +0800

    Create an object calculator with 3 methods

--- calculate.js
+++ calculate.js
@@ -8,3 +8,18 @@ sum() returns the sum of saved values.
 mul() multiplies saved values and returns the result.
 */
 
+let calculator = {
+  read() {
+    this.numA = +prompt("Enter the first number");
+    this.numB = +prompt("Enter the second number");
+  },
+
+  sum() {
+    return this.numA + this.numB;
+  },
+
+  mul() {
+    return this.numA * this.numB;
+  }
+};
+
