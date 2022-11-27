23abfa7714c187e2fba2535b815b6c11220ed6b8
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-28 07:41:20 +0800

    Create an object calculator with 3 methods

---
+++ calculate.js
@@ -0,0 +1,10 @@
+/*
+Create a calculator
+importance: 5
+Create an object calculator with three methods:
+
+read() prompts for two values and saves them as object properties with names a and b respectively.
+sum() returns the sum of saved values.
+mul() multiplies saved values and returns the result.
+*/
+
